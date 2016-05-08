91126f6635231dfdc1517db8bf07bfa899e86c1b
RuloKea <rulo@kea.dk>
2016-05-08 20:41:06 +0200

    added connection string

--- todotest/DAL/TodoContext.cs
+++ todotest/DAL/TodoContext.cs
@@ -10,5 +10,8 @@ namespace todotest.DAL
     public class TodoContext: DbContext
     {
         public DbSet<Todo> Todos { get; set; }
+        public TodoContext():base("DefaultConnection")
+        {
+        }
     }
 }
